7c895fd8033096e37b39a29ddb8c755f5d530178
hdd <664314822@qq.com>
2026-07-03 14:06:06 +0800

    fix: 修复 CCTreeHoleView viewModel 缺少 @State 导致 $viewModel 不可用

--- ChillCat/Presentation/Modules/TreeHole/Views/CCTreeHoleView.swift
+++ ChillCat/Presentation/Modules/TreeHole/Views/CCTreeHoleView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct CCTreeHoleView: View {
-    var viewModel: CCTreeHoleViewModel
+    @State var viewModel: CCTreeHoleViewModel
     @State private var showEmoji = false
     @State private var showContentWarning = false
     @State private var pendingPublishText: String = ""
